6e24a5f2283f504997b5657cd79bb55cadf408d7
vel22788 <158492077+vel22788@users.noreply.github.com>
2024-08-09 11:07:52 -0600

    Esclavo temp

---
+++ Esclavo_temp/Esclavo_temp/main.c
@@ -0,0 +1,16 @@
+/*
+ * main.c
+ *
+ * Created: 8/9/2024 11:07:13 AM
+ *  Author: Luis Angel Velasquez
+ */ 
+
+#include <xc.h>
+
+int main(void)
+{
+    while(1)
+    {
+        //TODO:: Please write your application code 
+    }
+}
